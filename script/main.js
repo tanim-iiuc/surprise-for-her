64daf0da2b2bc6803659fcbeca0c07f2cc7a8d23
tanim26072000 <tanim26072000@gmail.com>
2025-11-17 12:19:48 +0600

    fixed minor alignments

--- script/main.js
+++ script/main.js
@@ -1,22 +1,22 @@
 // trigger to play music in the background with sweetalert
-window.addEventListener('load', () => {
-    Swal.fire({
-        title: 'Do you want to play music in the background?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes',
-        cancelButtonText: 'No',
-    }).then((result) => {
-        if (result.isConfirmed) {
-            document.querySelector('.song').play();
-            animationTimeline();
-        } else {
-            animationTimeline();
-        }
-    });
-});
+// window.addEventListener('load', () => {
+//     Swal.fire({
+//         title: 'Do you want to play music in the background?',
+//         icon: 'warning',
+//         showCancelButton: true,
+//         confirmButtonColor: '#3085d6',
+//         cancelButtonColor: '#d33',
+//         confirmButtonText: 'Yes',
+//         cancelButtonText: 'No',
+//     }).then((result) => {
+//         if (result.isConfirmed) {
+//             document.querySelector('.song').play();
+//             animationTimeline();
+//         } else {
+//             animationTimeline();
+//         }
+//     });
+// });
 
 
 // animation timeline
@@ -38,7 +38,7 @@ const animationTimeline = () => {
     
     // Calculate the per-character delay to finish in 2 seconds
     // (2.0 seconds / number of characters)
-    const deleteStaggerDelay = 2.0 / chatboxSpans.length;
+    const deleteStaggerDelay = 1.0 / chatboxSpans.length;
 
     const ideaTextTrans = {
         opacity: 0,
@@ -310,3 +310,4 @@ const animationTimeline = () => {
         tl.restart();
     });
 }
+animationTimeline();
